c8b2ae1ed9528764e5da1ac4b6334043e6de9e24
Marcin Wronka <marcin.w@founderandlightning.com>
2023-11-30 15:53:54 +0100

    fixed sha f92a4a4

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -16,103 +16,103 @@ test('New users can register', function () {
 });
 
 test('Password format validations', function () {
-    $response_lower_case_password = $this->post(route('register'), [
+    $responseLowerCasePassword = $this->post(route('register'), [
         'first_name' => 'Test',
         'email' => 'test@founderandlightning.com',
         'password' => 'password',
         'password_confirmation' => 'password',
     ]);
 
-    $response_lower_case_password->assertInvalid();
+    $responseLowerCasePassword->assertInvalid();
 
-    $response_upper_case_password = $this->post(route('register'), [
+    $responseUpperCasePassword = $this->post(route('register'), [
         'first_name' => 'Test',
         'email' => 'test@founderandlightning.com',
         'password' => 'PASSWORD',
         'password_confirmation' => 'PASSWORD',
     ]);
 
-    $response_upper_case_password->assertInvalid();
+    $responseUpperCasePassword->assertInvalid();
 
-    $response_mixed_case_password = $this->post(route('register'), [
+    $responseMixedCasePassword = $this->post(route('register'), [
         'first_name' => 'Test',
         'email' => 'test@founderandlightning.com',
         'password' => 'PASSword',
         'password_confirmation' => 'PASSword',
     ]);
 
-    $response_mixed_case_password->assertInvalid();
+    $responseMixedCasePassword->assertInvalid();
 
-    $response_mixed_case_and_number_password = $this->post(route('register'), [
+    $responseMixedCaseAndNumberPassword = $this->post(route('register'), [
         'first_name' => 'Test',
         'email' => 'test@founderandlightning.com',
         'password' => 'PASSword1',
         'password_confirmation' => 'PASSword1',
     ]);
 
-    $response_mixed_case_and_number_password->assertInvalid();
+    $responseMixedCaseAndNumberPassword->assertInvalid();
 
-    $response_small_password = $this->post(route('register'), [
+    $responseSmallPassword = $this->post(route('register'), [
         'first_name' => 'Test',
         'email' => 'test@founderandlightning.com',
         'password' => 'Sword1@',
         'password_confirmation' => 'Sword1@',
     ]);
 
-    $response_small_password->assertInvalid();
+    $responseSmallPassword->assertInvalid();
 
-    $response_consecutive_chars_password = $this->post(route('register'), [
+    $responseConsecutiveCharsPassword = $this->post(route('register'), [
         'first_name' => 'Test',
         'email' => 'test@founderandlightning.com',
         'password' => 'P@sssword1',
         'password_confirmation' => 'P@sssword1',
     ]);
 
-    $response_consecutive_chars_password->assertInvalid();
+    $responseConsecutiveCharsPassword->assertInvalid();
 
-    $response_sequential_inc_chars_password = $this->post(route('register'), [
+    $responseSequentialIncCharsPassword = $this->post(route('register'), [
         'first_name' => 'Test',
         'email' => 'test@founderandlightning.com',
         'password' => 'P@ssword123',
         'password_confirmation' => 'P@sssword123',
     ]);
 
-    $response_sequential_inc_chars_password->assertInvalid();
+    $responseSequentialIncCharsPassword->assertInvalid();
 
-    $response_sequential_dec_chars_password = $this->post(route('register'), [
+    $responseSequentialDecCharsPassword = $this->post(route('register'), [
         'first_name' => 'Test',
         'email' => 'test@founderandlightning.com',
         'password' => 'P@ssword321',
         'password_confirmation' => 'P@sssword321',
     ]);
 
-    $response_sequential_dec_chars_password->assertInvalid();
+    $responseSequentialDecCharsPassword->assertInvalid();
 
-    $response_sequential_inc_keyboard_chars_password = $this->post(route('register'), [
+    $responseSequentialIncKeyboardCharsPassword = $this->post(route('register'), [
         'first_name' => 'Test',
         'email' => 'test@founderandlightning.com',
         'password' => 'P@ssword1qwer',
         'password_confirmation' => 'P@sssword1qwer',
     ]);
 
-    $response_sequential_inc_keyboard_chars_password->assertInvalid();
+    $responseSequentialIncKeyboardCharsPassword->assertInvalid();
 
-    $response_sequential_dec_keyboard_chars_password = $this->post(route('register'), [
+    $responseSequentialDecKeyboardCharsPassword = $this->post(route('register'), [
         'first_name' => 'Test',
         'email' => 'test@founderandlightning.com',
         'password' => 'P@ssword1/.,m',
         'password_confirmation' => 'P@sssword1/.,m',
     ]);
 
-    $response_sequential_dec_keyboard_chars_password->assertInvalid();
+    $responseSequentialDecKeyboardCharsPassword->assertInvalid();
 
-    $response_valid_password = $this->post(route('register'), [
+    $responseValidPassword = $this->post(route('register'), [
         'first_name' => 'Test',
         'email' => 'test@founderandlightning.com',
         'password' => 'PASSword@12-3',
         'password_confirmation' => 'PASSword@12-3',
     ]);
 
-    $response_valid_password->assertCreated();
-    $response_valid_password->assertSee(__('messages.user.registered'));
+    $responseValidPassword->assertCreated();
+    $responseValidPassword->assertSee(__('messages.user.registered'));
 });
