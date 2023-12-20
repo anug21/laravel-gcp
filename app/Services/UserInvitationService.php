8ee8351ae0bf7e5618b0cdd9f495c8a7f56575f4
Marcin Wronka <marcin.w@founderandlightning.com>
2023-12-20 14:23:05 +0100

    User invitation email sending

--- app/Services/UserInvitationService.php
+++ app/Services/UserInvitationService.php
@@ -2,8 +2,10 @@
 
 namespace App\Services;
 
+use App\Mail\UserInvitationMail;
 use App\Models\UserInvitation;
 use DateTime;
+use Illuminate\Support\Facades\Mail;
 use Spatie\Permission\Models\Role;
 
 class UserInvitationService
@@ -13,14 +15,19 @@ class UserInvitationService
         UserInvitation::where('email', $data['email'])->delete(); // invalidate previous by SoftDelete
 
         $expiration = (new DateTime())->modify('+' . config('constants.user.invitation_lifetime') . ' minutes');
+        $signature = bin2hex(openssl_random_pseudo_bytes(32));
 
         $invitation = new UserInvitation([
             'email' => $data['email'],
             'role_id' => Role::where('name', $data['role'])->first()->id,
-            'expires_at' => $expiration
+            'expires_at' => $expiration,
+            'signature' => $signature,
         ]);
 
         $invitation->save();
+
+        Mail::to($data['email'])->send(new UserInvitationMail(['signature' => $signature]));
+
         return $invitation;
     }
 }
