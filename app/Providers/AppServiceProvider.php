d470294ce5ab964ceb1b8cdb465cec965716f514
Marcin Wronka <marcin.w@founderandlightning.com>
2024-01-09 11:35:19 +0100

    Feature flags resolving on boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use Elastic\Elasticsearch\Client;
 use Elastic\Elasticsearch\ClientBuilder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Pennant\Feature;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,6 +31,17 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production') || $this->app->environment('staging')) {
             URL::forceScheme('https');
             $this->app['request']->server->set('HTTPS', 'on');
+
+            $this->features();
+        }
+    }
+
+    private function features(): void
+    {
+        // define global (artificial Pennant scope) features for the current user scope
+        $features = DB::select("SELECT * FROM features WHERE scope='__global'");
+        foreach ($features as $feature) {
+            Feature::define($feature->name, $feature->value === 'true');
         }
     }
 }
