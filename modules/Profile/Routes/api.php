0d03542008ea77c3e0dc1e8911a0c72817ca4f6d
jsdox <jatindersingh@founderandlightning.com>
2024-02-26 17:07:55 +0530

    change api pre-fix url for image and profile APIs

--- modules/Profile/Routes/api.php
+++ modules/Profile/Routes/api.php
@@ -5,24 +5,23 @@ use Modules\Profile\Http\Controllers\v1\ProfileImageController;
 use Modules\Profile\Http\Controllers\v1\ProfileSubscriptionController;
 use Modules\Profile\Http\Controllers\v1\ProfileNotificationController;
 
-Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+Route::middleware(['auth:sanctum', 'verified'])->prefix('api/v1/profile')->group(function () {
     Route::controller(ProfileController::class)
-        ->prefix('api/v1/profile')
         ->group(function () {
             Route::get('/', 'show')->name('profile.show');
             Route::patch('/', 'update')->name('profile.update');
             Route::delete('/', 'destroy')->name('profile.destroy');
         });
-    Route::get('/profile/subscription', ProfileSubscriptionController::class)
+    Route::get('subscription', ProfileSubscriptionController::class)
         ->name('profile-subscription.show');
-    Route::post('/profile/image', [ProfileImageController::class, 'store'])
+    Route::post('image', [ProfileImageController::class, 'store'])
         ->name('profile-image.store');
-    Route::delete('/profile/image', [ProfileImageController::class, 'destroy'])
+    Route::delete('image', [ProfileImageController::class, 'destroy'])
         ->name('profile-image.destroy');
-    Route::get('/profile/image/status', [ProfileImageController::class, 'status'])
+    Route::get('image/status', [ProfileImageController::class, 'status'])
         ->name('profile-image.status');
-    Route::get('/profile/notifications', [ProfileNotificationController::class, 'show'])
+    Route::get('notifications', [ProfileNotificationController::class, 'show'])
         ->name('profile-notification.show');
-    Route::patch('/profile/notifications', [ProfileNotificationController::class, 'update'])
+    Route::patch('notifications', [ProfileNotificationController::class, 'update'])
         ->name('profile-notification.update');
 });
