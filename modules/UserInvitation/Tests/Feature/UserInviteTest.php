23716cf07a83e3cc72ab3e933279f2d2e2254bf8
jsdox <jatindersingh@founderandlightning.com>
2024-03-11 16:41:28 +0530

    tests added for resend user invite

--- modules/UserInvitation/Tests/Feature/UserInviteTest.php
+++ modules/UserInvitation/Tests/Feature/UserInviteTest.php
@@ -4,11 +4,11 @@ uses(Tests\TestCase::class)->in( __DIR__ );
 
 test('User can resend invite immediately after signup - with route url', function () {
     $this->withHeader('Accept', 'application/json')
-        ->post('api/v1/invitations/resend/'. fake()->email)->assertOk();
+        ->post('api/v1/invitations/resend/'. fake()->safeEmail)->assertOk();
 });
 
 test('User can resend invite immediately after signup - with route name', function () {
     $this->withHeader('Accept', 'application/json')
-        ->post(route('invite.resend', ['email' => fake()->email]))
+        ->post(route('invite.resend', ['email' => fake()->safeEmail]))
         ->assertOk();
 });
